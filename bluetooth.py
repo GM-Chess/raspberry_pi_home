6d620ddffb78a97c58c0d7df8bb4c0aa6f031c0a
Curtis Jaekl <curtisjaekl@gmail.com>
2025-04-19 00:33:20 -0400

    add packing

--- bluetooth.py
+++ bluetooth.py
@@ -51,8 +51,8 @@ async def main():
                 await asyncio.sleep(1)
                 # send a command to the Pico
                 command = b"Hello from Pi Zero!"
-                await client.write_gatt_char(TEMP_CHAR_UUID, command)
-                print("Sent command to Pico:", command.decode())
+                await client.write_gatt_char(TEMP_CHAR_UUID, struct.pack("<h",command))
+                //print("Sent command to Pico:", command.decode())
                 # Wait for a while to see the response
                 await asyncio.sleep(1)
 
